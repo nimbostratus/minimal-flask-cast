cacf1202a4b7a8ddb57758122f12070c88e26a91
Sven Bröckling <sven@nimbostratus.de>
2014-05-12 11:27:37 +0200

    Added requirements.txt, questions and some jinja

--- cast/app.py
+++ cast/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+{% if sqlalchemy %}
+from flask.ext.sqlalchemy import SQLAlchemy
+{% endif %}
 
 app = Flask(__name__)
+{% if sqlalchemy %}
+db = SQLAlchemy()
+{% endif %}
+
 
 @app.route('/')
 def index():
